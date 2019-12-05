d400427362d88632d21dabfc7acdfc8e92f42123
Epharius <measteregg@gmail.com>
2019-12-05 21:24:13 +0100

    create base

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8" />
+		<link href="style.css" rel="stylesheet">
+		<title>La Caf- Accueil</title>
+	</head>
+	<body>
+		bite
+	</body>
+</html>
