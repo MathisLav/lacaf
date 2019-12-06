d0533e3a7c9e48b6b77a2adcd1146c1987c8faad
Mathis L <abc.live@live.fr>
2019-12-06 03:22:38 +0100

    Update script.js

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ function setup(){
 }
 
 function push_msg(){
-    msg = window.document.getElementById("input_msg").value;
+    let msg = window.document.getElementById("input_msg").value;
     if(msg != "") {
         window.document.getElementById("input_msg").value = '';
 
@@ -19,10 +19,14 @@ function push_msg(){
 }
 
 function add_msg(msg, side){
-    div_msg = document.createElement("div");
+    let div_msg = document.createElement("div");
     div_msg.classList.add("div_"+side);
     div_msg.appendChild(document.createTextNode(msg));
-    window.document.getElementById("msgs_chat").insertBefore(div_msg, window.document.getElementById("div_pending"));
+    //window.document.getElementById("msgs_chat").insertBefore(div_msg, window.document.getElementById("div_pending"));
+    window.document.getElementById("msgs_chat").appendChild(div_msg);
+
+    var element = document.getElementById("msgs_chat");
+    element.scrollTop = element.scrollHeight - element.clientHeight;
 }
 
  function bot_resp(msg){
@@ -30,17 +34,17 @@ function add_msg(msg, side){
     if(!resp)
         resp = random(json_resp["default"]);
 
-    window.document.getElementById("div_pending").textContent = "Gertrude est entrain d'ecrire";
-    compte ++;
+    window.document.getElementById("div_pending").textContent = "Gertrude est entrain d'ecrire...";
+    compte++;
         
     window.setTimeout(function(resp) { 
         add_msg(resp, "bot");
-        compte --;
+        compte--;
         if(compte <= 0){
             compte = 0;
             window.document.getElementById("div_pending").textContent = "";
         }
-    }, 2000, resp);
+    }, 1500, resp);
     // text.replaceData(0, text.length, "Gertrude : " + resp);
     // window.setTimeout(document.createTextNode, 1000, "Gertrude : " + msg);
     
